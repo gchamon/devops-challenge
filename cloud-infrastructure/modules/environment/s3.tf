d396c30f4dfe31c9c1f141da1dbf8cf8f03716a4
Gabriel Chamon Araujo <gchamon@live.com>
2020-06-06 20:02:40 -0300

    added frontend package-lock; changin frontend bucket name

--- cloud-infrastructure/modules/environment/s3.tf
+++ cloud-infrastructure/modules/environment/s3.tf
@@ -1,6 +1,6 @@
 module "s3_bucket_static" {
   source                   = "../website_bucket"
-  bucket                   = "${var.domain_name}-website"
+  bucket                   = "${var.environment_name}-website"
   domain_name              = var.domain_name
   origin_access_id_iam_arn = aws_cloudfront_origin_access_identity.main.iam_arn
 }
