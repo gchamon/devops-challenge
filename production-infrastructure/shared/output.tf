42caf9f5bb481f2988542466375f850650db3131
Gabriel Chamon Araujo <gchamon@live.com>
2020-06-06 14:08:29 -0300

    added route53_delegation_set output

--- production-infrastructure/shared/output.tf
+++ production-infrastructure/shared/output.tf
@@ -9,3 +9,9 @@ output "s3_bucket_ssl_keys" {
 output "route53_zone_root" {
   value = aws_route53_zone.root
 }
+
+output "route53_delegation_set" {
+  value = aws_route53_delegation_set.root
+}
+
+resource "null_resource" "refresh_output" {}
