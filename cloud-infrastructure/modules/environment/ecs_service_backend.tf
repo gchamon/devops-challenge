8b6b166746102e6bde9b6b2098b52837448c204e
Gabriel Chamon Araujo <gchamon@live.com>
2020-06-07 16:18:33 -0300

    adding correct health check

--- cloud-infrastructure/modules/environment/ecs_service_backend.tf
+++ cloud-infrastructure/modules/environment/ecs_service_backend.tf
@@ -17,6 +17,10 @@ module "ecs_service_backend" {
   lb_listener       = module.load_balancer.https_listener
   load_balancer     = module.load_balancer.load_balancer
   task_role_arn     = module.iam_role_ecs_backend_task.role.arn
+  health_check = {
+    path    = "/status"
+    matcher = 404
+  }
   containers = [
     {
       name              = "backend"
