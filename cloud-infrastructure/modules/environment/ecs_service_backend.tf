e5d1a014c5a033b34de1de0114120cbd7f6474f7
Gabriel Chamon Araujo <gchamon@live.com>
2020-06-07 14:43:06 -0300

    adding ecr deployment; ecs service is port 5000

--- cloud-infrastructure/modules/environment/ecs_service_backend.tf
+++ cloud-infrastructure/modules/environment/ecs_service_backend.tf
@@ -13,7 +13,7 @@ module "ecs_service_backend" {
   desired_count     = 2
   url               = var.domain_name
   lb_container_name = "backend"
-  lb_container_port = 80
+  lb_container_port = 5000
   lb_listener       = module.load_balancer.https_listener
   load_balancer     = module.load_balancer.load_balancer
   containers = [
@@ -24,7 +24,7 @@ module "ecs_service_backend" {
       soft-memory-limit = 64
       port-mappings = [
         {
-          container-port = 80
+          container-port = 5000
         }
       ]
     }
